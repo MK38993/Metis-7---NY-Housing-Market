9618a7543adb1cb493543d3c06bb3c266b59d32c
MK38993 <matthewkwee@icloud.com>
2021-12-16 17:54:17 -0500

    Update update_model.py

--- update_model.py
+++ update_model.py
@@ -11,12 +11,7 @@ from random import randint
 import re
 import datetime as dt
 
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.tag import pos_tag
-from nltk.corpus import stopwords
-from textblob import TextBlob
-import string
+
 
 import requests
 
@@ -28,13 +23,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 
 import os
-os.chdir('/Users/matthewkwee/Metis/Module 7 - NY Real Estate Regression')
-
-
-
-
-
-
+os.chdir('/Users/matthewkwee/Metis/Metis-7---Engineering')
 
 
 
@@ -49,26 +38,22 @@ f.close()
 print('Constructing Random Forest Regressor...')
 
 X = listing_frame_v8.drop(['price'], axis=1)
-regularizer=StandardScaler()
-X_reg = regularizer.fit_transform(X)
 
 y = listing_frame_v8['price']
 
-
 print('Fitting model...')
-model = RandomForestRegressor(n_estimators=256,max_features=1.0, random_state=420)
-model.fit(X_reg,y)
+model = RandomForestRegressor(n_estimators=96,max_features=1.0, random_state=420)
+model.fit(X,y)
 print('Scoring model...')
 
 # Get the r2 on the validation data
-predicted_prices = model.predict(X_reg)
+predicted_prices = model.predict(X)
 trf = r2_score(y , predicted_prices)
 print('Random forest r2 = ', trf)
 
 with open('RandomForestRegressor','wb') as f:
 	pickle.dump(model,f)
 f.close()
-
 
 os.chdir('/Users/matthewkwee/Metis/engi_res')
 

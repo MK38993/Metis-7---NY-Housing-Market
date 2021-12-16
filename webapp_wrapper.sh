a7c86ed65badebbda60de335c64d1aee36fbac97
MK38993 <matthewkwee@icloud.com>
2021-12-16 18:24:13 -0500

    Update webapp_wrapper.sh

--- webapp_wrapper.sh
+++ webapp_wrapper.sh
@@ -3,8 +3,8 @@
 source ~/.zprofile
 source ~/.zshrc
 
+cd Metis
+cd Metis-7---Engineering
+
 python webapp.py
 
-
-
-#http://127.0.0.1:5000/predict?bed_num=2&bath_num=2&sqft_num=2000&floor_num=2&room_num=6&year_num=2000&building_type=”Single-Family-Home”&borough=”Brooklyn”&description=Lovely+Description

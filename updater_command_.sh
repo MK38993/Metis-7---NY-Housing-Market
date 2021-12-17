76047b23573e635ec93b21d038d8121788338261
MK38993 <matthewkwee@icloud.com>
2021-12-16 23:42:49 -0500

    Updating materials

--- updater_command_.sh
+++ updater_command_.sh
@@ -18,18 +18,20 @@ python update_data.py
 
 #Update the RFR model.
 python update_model.py
-
-#Push to GitHub
-
+#Push to backup repository
 cd ~
 cd Metis
 cd engi_res
 git remote set-url origin https://MK38993:ghb_PlaceholderForMyAPIKeyItsSecretSorry@github.com/MK38993/engi_res
 git pull
-git add RandomForestRegressor
+git add RandomForestRegressor.bin
 git commit -a -m "Updated RFR"
-git add StandardScaler
-git commit -a -m "Updated StandardScaler"
 git push origin main
 
 
+#Update Heroku app
+cd ~
+cd nyc_housing
+git add .
+git commit -am "Updating resources"
+git push heroku master
